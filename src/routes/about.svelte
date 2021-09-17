aafd8735363afb20631cc9372ffab9d457631c0c
trevor <me@trevorbehnke.com>
2021-09-17 00:29:28 -0700

    v1 working

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<p>This is a really cool demo that uses that pokemon api to build a pokedex</p>
---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+// module.exports = {
+//   purge: [],
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+	mode: 'jit',
+	purge: ['./src/**/*.svelte']
+};
