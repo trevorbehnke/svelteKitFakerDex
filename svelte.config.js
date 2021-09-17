83cf5b4e6d1d7e0793132d78a41e464d6b0a61ad
trevor <me@trevorbehnke.com>
2021-09-17 00:45:52 -0700

    added netlify toml and dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 /** @type {import('@sveltejs/kit').Config} */
+const netlify = require('@sveltejs/adapter-netlify');
 const config = {
 	kit: {
+		adapter: netlify(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
